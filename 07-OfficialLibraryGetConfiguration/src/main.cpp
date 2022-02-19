1d23afdb3386a65d005c09532bc0136404600dd6
mostafa heidari <mostafa.hk@gmail.com>
2022-02-20 00:19:14 +0330

    fix

--- 07-OfficialLibraryGetConfiguration/src/main.cpp
+++ 07-OfficialLibraryGetConfiguration/src/main.cpp
@@ -1,77 +1,85 @@
 #include <Arduino.h>
 #include <otadrive_esp.h>
-#include <EEPROM.h>
 #include <ArduinoJson.h>
+#include <EEPROM.h>
 
-typedef struct configs
+struct configs
 {
-  uint32_t initialized_sign;
+  uint32_t sign;
   char ssid[33];
   char pass[33];
-  uint16_t on_time;
-  uint16_t off_time;
+  uint16_t blink_on;
+  uint16_t blink_off;
 };
 
-configs local_conf;
+configs local_configs;
 
 void setup()
 {
-  // load configs from EEPROM
+  OTADRIVE.setInfo("08ef4e5f-f062-40bb-b21d-fd7f13124609", "1.0.0");
+  Serial.begin(115200);
   EEPROM.begin(512);
-  EEPROM.get(0, local_conf);
-  if (local_conf.initialized_sign != 0x123456AB)
+  EEPROM.get(0, local_configs);
+
+  if (local_configs.sign != 0xA123B456)
   {
-    // load default configs
-    local_conf.initialized_sign = 0x123456AB;
-    strcpy(local_conf.ssid, "OTAdrive");
-    strcpy(local_conf.pass, "$OTAdrive#");
-    local_conf.on_time = 250;
-    local_conf.off_time = 250;
-    EEPROM.put(0, local_conf);
+    Serial.println("Load default configs");
+    strcpy(local_configs.ssid, "OTAdrive");
+    strcpy(local_configs.pass, "$OTAdrive#");
+    local_configs.blink_on = 250;
+    local_configs.blink_off = 350;
+    local_configs.sign = 0xA123B456;
+    EEPROM.put(0, local_configs);
+    EEPROM.commit();
+  }
+  else
+  {
+    Serial.println("Configs are valid");
   }
 
-  OTADRIVE.setInfo("bd076abe-a423-4880-85b3-4367d07c8eda", "1.0.0");
-  WiFi.begin(local_conf.ssid, local_conf.pass);
+  WiFi.begin(local_configs.ssid, local_configs.pass);
   pinMode(LED_BUILTIN, OUTPUT);
-  Serial.begin(115200);
 }
 
-void sync_otadrive()
+void sync_task()
 {
-  if (WiFi.status() != WL_CONNECTED)
-    return;
   static int sync_timer = 0;
   sync_timer++;
   if (sync_timer < 10)
     return;
   sync_timer = 0;
 
-  // do sync stuff here
+  if (WiFi.status() != WL_CONNECTED)
+    return;
+
+  // do sync and update operations here
   OTADRIVE.updateFirmware();
 
   String c = OTADRIVE.getConfigs();
-  Serial.printf("configs : %s\n", c.c_str());
-
-  // deserialize json configs
+  Serial.printf("config download: %s\n", c.c_str());
   StaticJsonDocument<250> doc;
   if (deserializeJson(doc, c) != DeserializationError::Ok)
   {
     Serial.println("deserialize error");
     return;
   }
-  strcpy(local_conf.ssid, doc["ssid"]);
-  strcpy(local_conf.pass, doc["pass"]);
-  local_conf.on_time = doc["blink_on"];
-  local_conf.off_time = doc["blink_off"];
-  EEPROM.put(0, local_conf);
+
+  Serial.printf("configs decode ssid=%s,blink_on=%d\n", (const char *)doc["ssid"], (int)doc["blink_on"]);
+
+  local_configs.blink_on = doc["blink_on"];
+  local_configs.blink_off = doc["blink_off"];
+  strcpy(local_configs.ssid, doc["ssid"]);
+  strcpy(local_configs.pass, doc["pass"]);
+
+  EEPROM.put(0, local_configs);
+  EEPROM.commit();
 }
 
 void loop()
 {
   digitalWrite(LED_BUILTIN, HIGH);
-  delay(local_conf.on_time);
+  delay(local_configs.blink_on);
   digitalWrite(LED_BUILTIN, LOW);
-  delay(local_conf.off_time);
-
-  sync_otadrive();
+  delay(local_configs.blink_off);
+  sync_task();
 }
