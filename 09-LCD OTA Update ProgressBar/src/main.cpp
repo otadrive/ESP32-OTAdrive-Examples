bdf00fb3dd1ceac2f41bb3ef774052f85da82f9b
mostafa heidari <mostafa.hk@gmail.com>
2022-05-04 09:13:22 +0430

    1.0.5

--- 09-LCD OTA Update ProgressBar/src/main.cpp
+++ 09-LCD OTA Update ProgressBar/src/main.cpp
@@ -11,22 +11,22 @@ const int pin_d7 = 14;
 const int pin_BL = -1;
 
 LiquidCrystal lcd(pin_RS, pin_EN, pin_d4, pin_d5, pin_d6, pin_d7);
-void OnProgress(int progress, int totalt);
+void OnProgress(int progress, int total);
 
 void setup()
 {
-  OTADRIVE.setInfo("888ac717-12f0-4da2-807a-c55bb70d1386", "1.0.4");
+  OTADRIVE.setInfo("888ac717-12f0-4da2-807a-c55bb70d1386", "1.0.5");
   OTADRIVE.onUpdateFirmwareProgress(OnProgress);
 
   lcd.begin(16, 2);
   lcd.setCursor(0, 0);
   lcd.print("OTAdrive");
   lcd.setCursor(0, 1);
-  lcd.print("App. V1.0.4");
+  lcd.print("App. V1.0.5");
   delay(1000);
   lcd.clear();
   lcd.setCursor(0, 0);
-  lcd.print("App. V1.0.4");
+  lcd.print("App. V1.0.5");
 
   WiFi.begin("OTAdrive", "@tadr!ve");
 }
@@ -44,14 +44,14 @@ void loop()
     lcd.setCursor(7, 1);
     lcd.print("WIFI");
 
-    if (OTADRIVE.timeTick(20))
+    if (OTADRIVE.timeTick(10))
     {
       auto inf = OTADRIVE.updateFirmwareInfo();
       if (inf.available)
       {
         lcd.clear();
         lcd.setCursor(0, 0);
-        lcd.printf("Upgrading %s", inf.version.c_str());
+        lcd.printf("Updating: %s", inf.version.c_str());
         OTADRIVE.updateFirmware();
       }
     }
@@ -59,20 +59,17 @@ void loop()
   delay(500);
 }
 
-int last = 0;
-void OnProgress(int progress, int totalt)
+void OnProgress(int progress, int total)
 {
-  int progressPercent = (100 * progress) / totalt;
-  Serial.print(".");
-  if (last != progressPercent && progressPercent % 10 == 0)
+  static int last_percent = 0;
+  int percent = (100 * progress) / total;
+  if (percent != last_percent)
   {
-    // print every 10%
     lcd.setCursor(0, 1);
-    for (uint8_t i = 0; i < progressPercent; i += 6)
-      lcd.print("=");
-    lcd.setCursor(6, 1);
-    lcd.printf("%d%%", progressPercent);
-    Serial.println(progressPercent);
+    for (uint8_t i = 0; i < percent; i++)
+      lcd.print("*");
+    lcd.setCursor(7, 1);
+    lcd.printf("%d%%", percent);
+    last_percent = percent;
   }
-  last = progressPercent;
 }
