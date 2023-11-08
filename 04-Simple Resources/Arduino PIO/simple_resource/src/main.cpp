48a69582e095acb44ae44690fc9f9b245ba65535
mostafa heidari <mostafa.hk@gmail.com>
2023-11-08 18:36:14 +0330

    resource

--- 04-Simple Resources/Arduino PIO/simple_resource/src/main.cpp
+++ 04-Simple Resources/Arduino PIO/simple_resource/src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <otadrive_esp.h>
+#include <WiFi.h>
 
 #define APIKEY "bd076abe-a423-4880-85b3-4367d07c8eda"//"COPY_APIKEY_HERE" // OTAdrive APIkey for this product
 #define FW_VER "v@1.2.3"          // this app version
@@ -13,8 +14,10 @@ int speed = 50;
 
 void setup()
 {
+  delay(2500);
   Serial.begin(115200);
   pinMode(LED, OUTPUT);
+  otd_log_i("Start application. Version %s, Serial: %s", FW_VER, OTADRIVE.getChipId().c_str());
 
   log_i("try connect wifi");
   WiFi.begin(WIFI_SSID, WIFI_PASS);
@@ -30,23 +33,20 @@ void setup()
 
   log_i("WiFi connected %s", WiFi.localIP().toString().c_str());
   OTADRIVE.setInfo(APIKEY, FW_VER);
-  OTADRIVE.useSSL(true);
 }
 
 void loop()
 {
-  log_i("Loop: Application version %s", FW_VER);
+  otd_log_i("Loop: Version %s, Serial: %s", FW_VER, OTADRIVE.getChipId().c_str());
   log_i("config parameter [speed]=%d", speed);
 
   if (WiFi.status() == WL_CONNECTED)
   {
-    WiFiClientSecure client;
-    client.setCACert(otadrv_ca);
     // Every 30 seconds
     if (OTADRIVE.timeTick(30))
     {
       // get latest config
-      configs = OTADRIVE.getConfigValues(client);
+      configs = OTADRIVE.getConfigValues();
       if (configs.containsKey("speed"))
       {
         speed = configs.value("speed").toInt();
