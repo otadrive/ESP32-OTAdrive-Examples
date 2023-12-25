d88b14661a9df9948dec620738e83efeec5d1e2e
mostafa heidari <mostafa.hk@gmail.com>
2023-12-25 07:01:45 +0330

    config

--- 02-Simple Config/Arduino PIO/simple_config/src/main.cpp
+++ 02-Simple Config/Arduino PIO/simple_config/src/main.cpp
@@ -1,15 +1,35 @@
 #include <Arduino.h>
 #include <otadrive_esp.h>
+#include <WiFi.h>
 
-#define APIKEY "bd076abe-a423-4880-85b3-4367d07c8eda"//"COPY_APIKEY_HERE" // OTAdrive APIkey for this product
-#define FW_VER "v@1.2.3"          // this app version
+/*
+Please define the following configuration in your product and assign it to the group of your device
+{
+    "speed":180,
+    "alarm":{
+        "number":"+188554436",
+        "msg1":"Fire, please help",
+        "msg2":"Emergency, please help"
+    }
+}
+*/
+
+#define APIKEY "bd076abe-a423-4880-85b3-4367d07c8eda" //"COPY_APIKEY_HERE" // OTAdrive APIkey for this product
+#define FW_VER "v@1.2.3"                              // this app version
 #define LED 2
-#define WIFI_SSID "OTAdrive2"
+#define WIFI_SSID "OTAdrive"
 #define WIFI_PASS "@tadr!ve"
 
 // put function declarations here:
 OTAdrive_ns::KeyValueList configs;
 int speed = 50;
+String alarm_num;
+String alarm_msg1;
+
+void printInfo()
+{
+  log_i("Application version %s, Serial:%s, IP:%s", FW_VER, OTADRIVE.getChipId().c_str(), WiFi.localIP().toString().c_str());
+}
 
 void setup()
 {
@@ -22,37 +42,46 @@ void setup()
   while (WiFi.status() != WL_CONNECTED)
   {
     log_i(".");
-    digitalWrite(2, HIGH);
+    digitalWrite(LED, HIGH);
     delay(100);
-    digitalWrite(2, LOW);
+    digitalWrite(LED, LOW);
     delay(400);
   }
 
   log_i("WiFi connected %s", WiFi.localIP().toString().c_str());
   OTADRIVE.setInfo(APIKEY, FW_VER);
-  OTADRIVE.useSSL(true);
 }
 
 void loop()
 {
-  log_i("Loop: Application version %s", FW_VER);
-  log_i("config parameter [speed]=%d", speed);
+  printInfo();
+  log_i("config parameter [speed]=%d,alarm %s -> %s", speed, alarm_num.c_str(), alarm_msg1.c_str());
 
   if (WiFi.status() == WL_CONNECTED)
   {
-    WiFiClientSecure client;
-    client.setCACert(otadrv_ca);
     // Every 30 seconds
     if (OTADRIVE.timeTick(30))
     {
       // get latest config
-      configs = OTADRIVE.getConfigValues(client);
+      configs = OTADRIVE.getConfigValues();
       if (configs.containsKey("speed"))
-      {
         speed = configs.value("speed").toInt();
-      }
+
+      if (configs.containsKey("alarm.number"))
+        alarm_num = configs.value("alarm.number");
+
+      if (configs.containsKey("alarm.msg1"))
+        alarm_msg1 = configs.value("alarm.msg1");
 
       // We don't talk about FOTA here. So code removed
+      // We don't talk about FOTA here. So code removed
+      // // retrive firmware info from OTAdrive server
+      // updateInfo inf = OTADRIVE.updateFirmwareInfo();
+      // log_d("\nfirmware info: %s ,%dBytes\n%s\n",
+      //       inf.version.c_str(), inf.size, inf.available ? "New version available" : "No newer version");
+      // // update firmware if newer available
+      // if (inf.available)
+      //   OTADRIVE.updateFirmware();
     }
   }
   delay(5000);
--- 03-JSON Config/Arduino PIO/json_config/src/main.cpp
+++ 03-JSON Config/Arduino PIO/json_config/src/main.cpp
@@ -1,16 +1,36 @@
+/*
+Please define the following configuration in your product and assign it to the group of your device
+{
+    "speed":180,
+    "alarm":{
+        "number":"+188554436",
+        "msg1":"Fire, please help",
+        "msg2":"Emergency, please help"
+    }
+}
+*/
+
+
 #include <Arduino.h>
 #include <otadrive_esp.h>
 #include <ArduinoJson.h>
+#include <WiFi.h>
 
-
-#define APIKEY "COPY_APIKEY_HERE" // OTAdrive APIkey for this product
-#define FW_VER "v@x.x.x" // this app version
+#define APIKEY "bd076abe-a423-4880-85b3-4367d07c8eda" // OTAdrive APIkey for this product
+#define FW_VER "v@21.3.5"                             // this app version
 #define LED 2
-#define WIFI_SSID "OTAdrive2"
+#define WIFI_SSID "OTAdrive"
 #define WIFI_PASS "@tadr!ve"
 
 // put function declarations here:
 int speed = 50;
+String alarm_num;
+String alarm_msg1;
+
+void printInfo()
+{
+  log_i("Application version %s, Serial:%s, IP:%s", FW_VER, OTADRIVE.getChipId().c_str(), WiFi.localIP().toString().c_str());
+}
 
 void setup()
 {
@@ -23,9 +43,9 @@ void setup()
   while (WiFi.status() != WL_CONNECTED)
   {
     log_i(".");
-    digitalWrite(2, HIGH);
+    digitalWrite(LED, HIGH);
     delay(100);
-    digitalWrite(2, LOW);
+    digitalWrite(LED, LOW);
     delay(400);
   }
 
@@ -35,8 +55,8 @@ void setup()
 
 void loop()
 {
-  log_i("Loop: Application version %s", FW_VER);
-  log_i("config parameter [speed]=%d", speed);
+  printInfo();
+  log_i("config parameter [speed]=%d,alarm %s -> %s", speed, alarm_num.c_str(), alarm_msg1.c_str());
 
   if (WiFi.status() == WL_CONNECTED)
   {
@@ -47,12 +67,28 @@ void loop()
       String payload = OTADRIVE.getJsonConfigs();
       DynamicJsonDocument doc(512);
       deserializeJson(doc, payload);
+
+      // extract values
       if (doc.containsKey("speed"))
-      {
         speed = doc["speed"].as<int>();
+
+      if (doc.containsKey("alarm"))
+      {
+        if (doc["alarm"].containsKey("number"))
+          alarm_num = doc["alarm"]["number"].as<String>();
+
+        if (doc["alarm"].containsKey("msg1"))
+          alarm_msg1 = doc["alarm"]["msg1"].as<String>();
       }
 
       // We don't talk about FOTA here. So code removed
+      // // retrive firmware info from OTAdrive server
+      // updateInfo inf = OTADRIVE.updateFirmwareInfo();
+      // log_d("\nfirmware info: %s ,%dBytes\n%s\n",
+      //       inf.version.c_str(), inf.size, inf.available ? "New version available" : "No newer version");
+      // // update firmware if newer available
+      // if (inf.available)
+      //   OTADRIVE.updateFirmware();
     }
   }
   delay(5000);
