2a9f9344b545fd50b017e51ba1b1797ec80a1c71
mostafa heidari <mostafa.hk@gmail.com>
2024-03-27 03:27:12 +0330

    config example

---
+++ 02-Simple_Config/ESP-IDF/simple_config/main/main.c
@@ -0,0 +1,283 @@
+#include <string.h>
+#include <inttypes.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "esp_system.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "esp_ota_ops.h"
+#include "esp_crt_bundle.h"
+#include "esp_http_client.h"
+#include "esp_https_ota.h"
+#include "nvs.h"
+#include "nvs_flash.h"
+
+#include <otadrive_esp.h>
+#include "esp_wifi.h"
+
+static const char *TAG = "simple_config";
+#define EXAMPLE_NETIF_DESC_STA "WiFi-IDF"
+
+// ===== WiFi Credentials
+#define WIFI_SSID "OTAdrive"
+#define WIFI_PASS "@tadr!ve"
+
+// ===== OTAdrive configurations
+#define OTADRIVE_APIKEY "bd076abe-a423-4880-85b3-4367d07c8eda"
+#define APP_VERSION "v@2.2.1.5"
+
+// ===== Some device parameters
+int speed = 100;
+char alarm_num[64];
+char alarm_msg1[64];
+char alarm_msg2[64];
+
+static esp_netif_t *s_example_sta_netif = NULL;
+static SemaphoreHandle_t s_semph_get_ip_addrs = NULL;
+static int s_retry_num = 0;
+
+esp_err_t example_wifi_connect(void);
+
+// ========================== OTA section
+static void otadrive_event_handler(void *arg, esp_event_base_t event_base,
+                                   int32_t event_id, void *event_data)
+{
+    if (event_base == ESP_HTTPS_OTA_EVENT)
+    {
+        switch (event_id)
+        {
+        case ESP_HTTPS_OTA_START:
+            ESP_LOGI(TAG, "OTA started");
+            break;
+        case ESP_HTTPS_OTA_CONNECTED:
+            ESP_LOGI(TAG, "Connected to server");
+            break;
+        case ESP_HTTPS_OTA_GET_IMG_DESC:
+            ESP_LOGI(TAG, "Reading Image Description");
+            break;
+        case ESP_HTTPS_OTA_VERIFY_CHIP_ID:
+            ESP_LOGI(TAG, "Verifying chip id of new image: %d", *(esp_chip_id_t *)event_data);
+            break;
+        case ESP_HTTPS_OTA_DECRYPT_CB:
+            ESP_LOGI(TAG, "Callback to decrypt function");
+            break;
+        case ESP_HTTPS_OTA_WRITE_FLASH:
+            ESP_LOGD(TAG, "Writing to flash: %d written", *(int *)event_data);
+            break;
+        case ESP_HTTPS_OTA_UPDATE_BOOT_PARTITION:
+            ESP_LOGI(TAG, "Boot partition updated. Next Partition: %d", *(esp_partition_subtype_t *)event_data);
+            break;
+        case ESP_HTTPS_OTA_FINISH:
+            ESP_LOGI(TAG, "OTA finish");
+            break;
+        case ESP_HTTPS_OTA_ABORT:
+            ESP_LOGI(TAG, "OTA abort");
+            break;
+        }
+    }
+}
+
+void save_configs()
+{
+    // save configs from EEPROM or where ever
+}
+
+void load_configs()
+{
+    // load configs from EEPROM or where ever
+}
+
+void renew_configs()
+{
+    // download configs
+    if (otadrive_downloadConfigValues())
+    {
+        ESP_LOGI(TAG, "Configs downloaded");
+        // lets decode and save config values
+        char n_buf[10];
+        if (otadrive_getConfigValue("speed", n_buf, sizeof(n_buf)))
+        {
+            ESP_LOGI(TAG, "configuration parameter [speed]: %s", n_buf);
+            speed = atoi(n_buf);
+        }
+        if (otadrive_getConfigValue("alarm.number", alarm_num, sizeof(alarm_num)))
+            ESP_LOGI(TAG, "configuration parameter [alarm.number]: %s", alarm_num);
+        if (otadrive_getConfigValue("alarm.msg1", alarm_msg1, sizeof(alarm_msg1)))
+            ESP_LOGI(TAG, "configuration parameter [alarm.msg1]: %s", alarm_msg1);
+        if (otadrive_getConfigValue("alarm.msg2", alarm_msg2, sizeof(alarm_msg2)))
+            ESP_LOGI(TAG, "configuration parameter [alarm.msg2]: %s", alarm_msg2);
+
+        save_configs();
+    }
+}
+
+void otadrive_thread(void *pvParameter)
+{
+    while (1)
+    {
+        if (otadrive_timeTick(30))
+        {
+            renew_configs();
+
+            otadrive_result r = otadrive_updateFirmwareInfo();
+            ESP_LOGI(TAG, "Check new OTA result %d,%lu", r.code, r.size);
+            if (r.code == OTADRIVE_NewFirmwareExists)
+            {
+                ESP_LOGI(TAG, "Lets download new firmware %s,%luBytes. Current firmware is %s",
+                         r.version, r.size, otadrive_currentversion());
+
+                r = otadrive_updateFirmware(false);
+                if (r.code == OTADRIVE_Success)
+                {
+                    // do something to prepare device for reboot
+                    esp_restart();
+                }
+
+                ESP_LOGI(TAG, "OTA Download Result %d", r.code);
+            }
+        }
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+}
+
+void app_main(void)
+{
+    ESP_LOGI(TAG, "app start");
+    // Initialize NVS.
+    esp_err_t err = nvs_flash_init();
+    if (err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND)
+    {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        err = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(err);
+
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    esp_wifi_set_ps(WIFI_PS_NONE);
+    ESP_ERROR_CHECK(example_wifi_connect());
+
+    load_configs();
+    // OTAdrive initialize
+    esp_event_handler_register(OTADRIVE_EVENTS, ESP_EVENT_ANY_ID, &otadrive_event_handler, NULL); // Register a handler to get updates on progress
+    otadrive_setInfo(OTADRIVE_APIKEY, APP_VERSION);
+
+    xTaskCreate(&otadrive_thread, "otadrive_example_task", 1024 * 16, NULL, 5, NULL);
+}
+
+// ========================== Wifi section
+bool example_is_our_netif(const char *prefix, esp_netif_t *netif)
+{
+    return strncmp(prefix, esp_netif_get_desc(netif), strlen(prefix) - 1) == 0;
+}
+
+static void example_handler_on_wifi_disconnect(void *arg, esp_event_base_t event_base,
+                                               int32_t event_id, void *event_data)
+{
+    s_retry_num++;
+    if (s_retry_num > 6)
+    {
+        ESP_LOGI(TAG, "WiFi Connect failed %d times, stop reconnect.", s_retry_num);
+        /* let example_wifi_sta_do_connect() return */
+        if (s_semph_get_ip_addrs)
+        {
+            xSemaphoreGive(s_semph_get_ip_addrs);
+        }
+        return;
+    }
+    ESP_LOGI(TAG, "Wi-Fi disconnected, trying to reconnect...");
+    esp_err_t err = esp_wifi_connect();
+    if (err == ESP_ERR_WIFI_NOT_STARTED)
+    {
+        return;
+    }
+    ESP_ERROR_CHECK(err);
+}
+
+static void example_handler_on_sta_got_ip(void *arg, esp_event_base_t event_base,
+                                          int32_t event_id, void *event_data)
+{
+    s_retry_num = 0;
+    ip_event_got_ip_t *event = (ip_event_got_ip_t *)event_data;
+    if (!example_is_our_netif(EXAMPLE_NETIF_DESC_STA, event->esp_netif))
+    {
+        return;
+    }
+    ESP_LOGI(TAG, "Got IPv4 event: Interface \"%s\" address: " IPSTR, esp_netif_get_desc(event->esp_netif), IP2STR(&event->ip_info.ip));
+    if (s_semph_get_ip_addrs)
+    {
+        xSemaphoreGive(s_semph_get_ip_addrs);
+    }
+    else
+    {
+        ESP_LOGI(TAG, "- IPv4 address: " IPSTR ",", IP2STR(&event->ip_info.ip));
+    }
+}
+
+void example_wifi_start(void)
+{
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    esp_netif_inherent_config_t esp_netif_config = ESP_NETIF_INHERENT_DEFAULT_WIFI_STA();
+    // Warning: the interface desc is used in tests to capture actual connection details (IP, gw, mask)
+    esp_netif_config.if_desc = EXAMPLE_NETIF_DESC_STA;
+    esp_netif_config.route_prio = 128;
+    s_example_sta_netif = esp_netif_create_wifi(WIFI_IF_STA, &esp_netif_config);
+    esp_wifi_set_default_wifi_sta_handlers();
+
+    ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK(esp_wifi_start());
+}
+
+esp_err_t example_wifi_sta_do_connect(wifi_config_t wifi_config, bool wait)
+{
+    if (wait)
+    {
+        s_semph_get_ip_addrs = xSemaphoreCreateBinary();
+        if (s_semph_get_ip_addrs == NULL)
+        {
+            return ESP_ERR_NO_MEM;
+        }
+    }
+    s_retry_num = 0;
+    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, WIFI_EVENT_STA_DISCONNECTED, &example_handler_on_wifi_disconnect, NULL));
+    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &example_handler_on_sta_got_ip, NULL));
+
+    ESP_LOGI(TAG, "Connecting to %s...", wifi_config.sta.ssid);
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
+    esp_err_t ret = esp_wifi_connect();
+    if (ret != ESP_OK)
+    {
+        ESP_LOGE(TAG, "WiFi connect failed! ret:%x", ret);
+        return ret;
+    }
+    if (wait)
+    {
+        ESP_LOGI(TAG, "Waiting for IP(s)");
+
+        xSemaphoreTake(s_semph_get_ip_addrs, portMAX_DELAY);
+
+        if (s_retry_num > 6)
+        {
+            return ESP_FAIL;
+        }
+    }
+    return ESP_OK;
+}
+
+esp_err_t example_wifi_connect(void)
+{
+    ESP_LOGI(TAG, "Start wifi_connect.");
+    example_wifi_start();
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = WIFI_SSID,
+            .password = WIFI_PASS,
+        },
+    };
+    return example_wifi_sta_do_connect(wifi_config, true);
+}
